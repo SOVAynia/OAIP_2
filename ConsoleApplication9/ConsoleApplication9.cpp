1fb64f8c71f8860220ea801a4d5be54153076b8f
SOVAynia <allregey@gmail.com>
2026-04-06 11:14:57 +0300

    Update ConsoleApplication9.cpp

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -1,23 +1,23 @@
 ﻿#include <iostream>
-using namespace std;
+using namespace std;  // используемые библиотеки
 
 // кратно ли числу A сумма цифра двузначного числа
 
 int main() {
-    int number;
+    int number;  // Ввод данных
     cin >> number;
 
     number = (number);
 
-    if (number < 10 || number > 99)
+    if (number < 10 || number > 99) //Проверка на двухзначоность числа
     {
-        cout << "Не двухзначное число." << endl; //Проверка на двухзначоность числа
+        cout << "Не двухзначное число." << endl; //Вывод ответа в случае если число не двухзначное
         return 0;
     }
 
     if (number % 3 == 0)
-        cout << "Равно 3" << endl;      //проверка кратности числа
-    else
+        cout << "Равно 3" << endl;      //Вывод ответа 
+    else                                //проверка кратности числа
         cout << "Не равно 3" << endl;
 
     return 0;
